540c10f80a24623255981e3ffacfe242a3d948fb
Drew Anello <drew.anello@gmail.com>
2023-01-08 12:55:13 -0500

    data populating on page

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 console.log('connected')
 
-const URL = 'https://www.omdbapi.com/?apikey=151680d4&t=titan'
+const URL = 'https://www.omdbapi.com/?apikey=151680d4&t=frozen'
 // console.log(URL)
 
 ///////////////////////
 //  cached elements/ element reference
 /////////////////////
+const titleEl = document.getElementById('title')
+const yearEl = document.getElementById('year')
+const ratedEl = document.getElementById('rated')
 fetch(URL)
   .then(function (response) {
     return response.json()
   })
   .then(function (data) {
+    titleEl.textContent = data.Title
+    yearEl.textContent = data.Year
+    ratedEl.textContent = data.Rated
     console.log(data)
   })
   .catch(function (error) {
