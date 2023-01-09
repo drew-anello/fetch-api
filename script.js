556d24dbc59b4f8cbb52f3ca46bc7ccdbb7142d3
Drew Anello <drew.anello@gmail.com>
2023-01-09 18:23:50 -0500

    adds event listener to form el

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('connected')
 
-const URL = 'https://www.omdbapi.com/?apikey=151680d4&t=frozen'
+const URL = 'https://www.omdbapi.com/?apikey=151680d4&t='
 // console.log(URL)
 
 ///////////////////////
@@ -9,19 +9,28 @@ const URL = 'https://www.omdbapi.com/?apikey=151680d4&t=frozen'
 const titleEl = document.getElementById('title')
 const yearEl = document.getElementById('year')
 const ratedEl = document.getElementById('rated')
-fetch(URL)
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (data) {
-    titleEl.textContent = data.Title
-    yearEl.textContent = data.Year
-    ratedEl.textContent = data.Rated
-    console.log(data)
-  })
-  .catch(function (error) {
-    console.log('were broken')
-  })
+const formEl = document.querySelector('form')
+const inp = document.getElementById('imp')
+
+formEl.addEventListener('click', handleGetData)
+
+function handleGetData (e) {
+  e.preventDefault()
+  console.log(e.target)
+  fetch(URL)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      titleEl.textContent = data.Title
+      yearEl.textContent = data.Year
+      ratedEl.textContent = data.Rated
+      console.log(data)
+    })
+    .catch(function (error) {
+      console.log('were broken')
+    })
+}
 ///////////////////////
 //  Event listeners
 /////////////////////
